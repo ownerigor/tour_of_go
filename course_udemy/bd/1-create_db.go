33abbf7a499026b3da12966ca93513315c5107af
Igor <89994712+ownerigor@users.noreply.github.com>
2025-06-14 16:53:39 -0300

    Creating SQLite Database

---
+++ course_udemy/bd/1-create_db.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name TEXT
+	);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Tabela de 'users' foi criada.")
+}
