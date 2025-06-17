5b39d1b6c79faa13243ff2f6d4216adfd170f967
Igor <89994712+ownerigor@users.noreply.github.com>
2025-06-16 21:08:31 -0300

    Modified `5-users.go`

--- course_udemy/bd/5-users.go
+++ course_udemy/bd/5-users.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/google/uuid"
@@ -37,6 +38,63 @@ func createTable(db *sql.DB) error {
 	return err
 }
 
+func insertUser(db *sql.DB, user *User) error {
+	stmt, err := db.Prepare("INSERT INTO users (id, name, email, age) VALUES ($1, $2, $3, $4)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(user.ID, user.Name, user.Email, user.Age)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateUser(db *sql.DB, user *User) error {
+	stmt, err := db.Prepare("UPDATE users SET name = ?, email = ?, age = ? WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(user.Name, user.Email, user.Age, user.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func selectUsers(db *sql.DB) ([]User, error) {
+	rows, err := db.Query("SELECT id, name, email, age FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var users []User
+	for rows.Next() {
+		var u User
+		err = rows.Scan(&u.ID, &u.Name, &u.Email, &u.Age)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+	return users, nil
+}
+
+func deleteUser(db *sql.DB, id string) error {
+	stmt, err := db.Prepare("DELETE FROM users where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	db, err := sql.Open("sqlite3", "./app.db")
 	if err != nil {
@@ -46,6 +104,36 @@ func main() {
 
 	//Criar a tabela
 	err = createTable(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Inserção de usuário
+	user := newUser("John Doe", "john.doe@example.com", 30)
+	err = insertUser(db, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Alterando usuário
+	user.Name = "John Smith"
+	user.Age = 31
+	err = updateUser(db, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Listagem de usuários
+	users, err := selectUsers(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, u := range users {
+		fmt.Printf("Usuário %s possui email %s e possui %d anos\n", u.Name, u.Email, u.Age)
+	}
+
+	//Exclusão de dados
+	err = deleteUser(db, user.ID)
 	if err != nil {
 		log.Fatal(err)
 	}
